79e1274102233755a2bf04ed3f661ebf2befaba9
Gustavo Sales <gus@thonlabs.io>
2025-01-21 14:08:40 -0300

    chore: removes gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const gulpUglify = require('gulp-uglify');
-
-
-function uglify() {
-  return gulp.src('dist/**/*.js').pipe(gulpUglify()).pipe(gulp.dest('dist/'));
-}
-
-exports.postBuild = gulp.series(uglify);
