a140ec3417077703c88bf82dcad94530ee7dec32
Gustavo Sales <gus@thonlabs.io>
2025-02-12 21:55:51 -0300

    fix: add suspense for toaster bc of useSearchParams

--- src/v15/pages/components/toaster-observable-wrapper.tsx
+++ src/v15/pages/components/toaster-observable-wrapper.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import React from "react";
 import ToasterObservable from "./toaster-observable";
 import {Toaster} from "../../../ui/components/toaster";
 
 export default function ToasterObservableWrapper() {
   return (
-    <>
+    <React.Suspense>
       <ToasterObservable />
       <Toaster />
-    </>
+    </React.Suspense>
   );
 }
