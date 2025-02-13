edb669266c62892820d1fdeef9543f638b9914f3
Gustavo Sales <gus@thonlabs.io>
2025-02-12 21:56:47 -0300

    feat: add toaster to v14

--- src/v14/pages/components/toaster-observable-wrapper.tsx
+++ src/v14/pages/components/toaster-observable-wrapper.tsx
@@ -1,12 +1,14 @@
-'use client';
+"use client";
 
-import React from 'react';
-import ToasterObservable from './toaster-observable';
+import React from "react";
+import ToasterObservable from "./toaster-observable";
+import {Toaster} from "../../../ui/components/toaster";
 
 export default function ToasterObservableWrapper() {
   return (
     <React.Suspense>
       <ToasterObservable />
+      <Toaster />
     </React.Suspense>
   );
 }
