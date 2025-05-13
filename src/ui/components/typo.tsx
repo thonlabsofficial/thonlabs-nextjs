07aee5802f446c8d79ed822606063ce8bbf931c0
Gustavo Sales <gustavo@gsales.io>
2025-05-12 21:50:52 -0400

    updates to use react 19 types

--- src/ui/components/typo.tsx
+++ src/ui/components/typo.tsx
@@ -41,7 +41,7 @@ const typoVariants = cva('font-sans text-text', {
 export interface TypoProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof typoVariants> {
-  as?: keyof JSX.IntrinsicElements;
+  as?: keyof React.JSX.IntrinsicElements;
   fallback?: React.ReactNode;
 }
 
