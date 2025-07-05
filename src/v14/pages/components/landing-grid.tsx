85886d3f957474daceb67a5bfa957dcc546f4623
Gustavo Sales <gus@thonlabs.io>
2025-07-05 20:18:15 -0300

    applies missing classes

--- src/v14/pages/components/landing-grid.tsx
+++ src/v14/pages/components/landing-grid.tsx
@@ -1,12 +1,12 @@
-import {cn} from "../../../ui/core/utils";
-import styles from "../../../shared/styles/landing-grid.module.css";
+import { cn } from '../../../ui/core/utils';
+import styles from '../../../shared/styles/landing-grid.module.css';
 
 export default function LandingGrid() {
   return (
     <div
       className={cn(
         `
-          absolute inset-0 z-[-1] -left-px opacity-60 
+          tl-absolute tl-inset-0 tl-z-[-1] tl-left-[-1px] tl-opacity-60 
           [mask-image:linear-gradient(transparent_1%,white,transparent_43%)]
         `,
         styles.landingGrid
