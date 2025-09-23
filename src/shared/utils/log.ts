fc27939793912387b0815435f889d02b8e93cd54
Gustavo Sales <gus@thonlabs.io>
2025-09-22 23:44:58 -0300

    standarize logs

--- src/shared/utils/log.ts
+++ src/shared/utils/log.ts
@@ -9,7 +9,7 @@ const Log = {
 	},
 	error(data: any) {
 		const timestamp = this.generateTimestamp();
-		console.error(`[${timestamp}] TL_ERROR: ${JSON.stringify(data)}`);
+		console.log(`[${timestamp}] TL_ERROR: ${JSON.stringify(data)}`);
 	},
 };
 
